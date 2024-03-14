18c4c4d24e3e211efb24494b10556dcb8cea885e
askar <maeaxvx@gmail.com>
2024-03-14 16:44:16 +0200

    changing style

--- gym_project/_project/user/admin.py
+++ gym_project/_project/user/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Sale, Payment
+from .models import Sale, Payment, Client, RecentAction
+
 
 admin.site.register(Sale)
 admin.site.register(Payment)
+admin.site.register(Client)
+admin.site.register(RecentAction)
